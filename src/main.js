286a27fc9b18590e787f232707a6c9ac71385cb4
tittieth <114925706+tittieth@users.noreply.github.com>
2022-12-09 10:24:07 +0100

    JavaScript påbörjat. Går att lägga till todos

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style/style.scss';
 
 /*
 [X] Användaren ska kunna skriva in sitt namn som sparas i localstorage
-[] Ska kunna lägga till todos och välja kategori och datum
+[X] Ska kunna lägga till todos och välja kategori och datum
 [] Ska kunna ta bort todos 
 [] Ska kunna redigera todos 
 [] Ska kunna markera en todo som klar och då läggs den till i en lista med klara 
@@ -14,33 +14,122 @@ import './style/style.scss';
 [] Deadline inom 5 dagar ska visas med text/färg 
 */
 
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+// Har utgått från en video av Tyler Potts och ändrat och lagt till lite saker.
+
+let newTodos = JSON.parse(localStorage.getItem('newTodos')) || [];
+let activeTodos = JSON.parse(localStorage.getItem('activeTodos')) || [];
+let completedTodos = JSON.parse(localStorage.getItem('completedTodos')) || [];
 const newTodoForm = document.querySelector('#new-todo-form');
-const nameInput = document.querySelector('#name');
-const username = localStorage.getItem('username') || '';
+  const nameInput = document.querySelector('#name');
+  const username = localStorage.getItem('username') || '';
 
-// För att spara användarens namn
-nameInput.value = username;
+window.addEventListener('load', () => {
+  // För att spara användarens namn
+  nameInput.value = username;
 
-nameInput.addEventListener('change', e => {
-  localStorage.setItem('username', e.target.value);
+  nameInput.addEventListener('change', e => {
+    localStorage.setItem('username', e.target.value);
+  })
+
+  // Tar användarens inputs och lägger in det i listan todos 
+  newTodoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const todo = {
+      content: e.target.elements.content.value,
+      dueDate: e.target.elements.dueDate.value,
+      category: e.target.elements.category.value,
+      done: false,
+      createdAt: new Date().getTime()
+    }
+
+    newTodos.push(todo);
+
+    localStorage.setItem('newTodos', JSON.stringify(newTodos));
+
+    e.target.reset();
+
+    DisplayTodos();
+  });
+  DisplayTodos();
 })
 
-// Tar användarens inputs och lägger in det i listan todos 
-newTodoForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+// Funktion för att skriva ut användarens todo
+function DisplayTodos() {
+  const todoList = document.querySelector('#todo-list');
 
-  const todo = {
-    content: e.target.elements.content.value,
-    dueDate: e.target.elements.dueDate.value,
-    category: e.target.elements.category.value,
-    done: false,
-    createdAt: new Date().getTime()
-  }
+  todoList.innerHTML = '';
 
-  todos.push(todo);
+  newTodos.forEach(todo => {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item')
 
-  localStorage.setItem('todos', JSON.stringify(todos));
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const content = document.createElement('div');
+    const dueDate = document.createElement('div');
+    const category = document.createElement('span');
+    const actions = document.createElement('div');
+    const edit = document.createElement('button');
+    const deleteButton = document.createElement('button');
 
-  e.target.reset();
-})
+    input.type = 'checkbox';
+    input.checked = todo.done; 
+    span.classList.add('bubble');
+
+    if (todo.category == 'personal') {
+      span.classList.add('personal');
+    }
+    if (todo.classList == 'kids') {
+      span.classList.add('kids');
+    }
+    if (todo.classList == 'job') {
+      span.classList.add('job');
+    } else {
+      span.classList.add('other');
+    }
+
+    content.classList.add('todo-content');
+    dueDate.classList.add('dueDate-div');
+    category.classList.add('category-icon');
+    actions.classList.add('actions');
+    edit.classList.add('edit');
+    deleteButton.classList.add('delete');
+
+    dueDate.innerHTML = todo.dueDate;
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+    edit.innerHTML = 'Edit';
+    deleteButton.innerHTML = 'Delete';
+
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    actions.appendChild(deleteButton);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+    todoItem.appendChild(dueDate);
+
+    todoList.appendChild(todoItem);
+
+    if (todo.done) {
+      todoItem.classList.add('done');
+    }
+
+    input.addEventListener('click', (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem('newTodos', JSON.stringify(newTodos));
+
+      if (todo.done) {
+        todoItem.classList.add('done');
+      } else {
+        todoItem.classList.remove('done');
+      };
+
+      DisplayTodos();
+    })
+  })
+}
+
+console.table(newTodos)
